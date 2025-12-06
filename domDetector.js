3588efb4d93357756c5035f26fb3a69ed34cfba7
Obodo <aobodo3@gmail.com>
2025-12-06 14:21:34 +0000

    Update domDetector.js

--- domDetector.js
+++ domDetector.js
@@ -1,2 +1,30 @@
-alert("Testing this function");
-document.body.style.border="5px solid red";
+//These make the webpage to display url and cookie infor
+//and the string
+console.log("Testing this function");
+//alert(document.URL); //displays a pop up on browser
+//alert(document.cookie); //displays a pop up on browser
+
+function regExpDetection(){
+
+    /* checks if the script tag is present using regular expression
+    i means incase sensitive
+    \s* means to match 0 or more space characters around the script tag
+    'script' means to match script
+    '< match opening angle bracket' 
+    '[^>]*' match any character except'>'
+    '>' match closing angle bracket*/
+    //expression for script tag
+    const CaseInsensitivesimpleRegEx=/<\s*script\s*[^>]*\/?>/i;
+
+    const input='<    script type="text/javascript" >';//input of a string
+
+    //returns true or false value if the script tag exists
+    /*checks if the regular expressions are in the 
+    string with 0 or more changes to RegEx */
+    return CaseInsensitivesimpleRegEx.test(input);
+}
+//displays result
+let checker=regExpDetection();
+console.log(checker);
+
+
