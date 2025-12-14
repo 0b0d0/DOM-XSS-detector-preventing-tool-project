1c61a01b0c0f5607fd67a5a99ca5378776869c3a
Obodo <aobodo3@gmail.com>
2025-12-14 18:14:03 +0000

    Update domDetector.js
    This includes a function to get data that contains the dom xss payloads from the website URL. This also includes a function to detect for payloads that has not been completed yet.

--- domDetector.js
+++ domDetector.js
@@ -1,33 +1,49 @@
+/*this fetches dom xss payloads using fetch command.*/
 
-/*common sinks list where payload can be executed*/
-const commonSinks=["alert","document\\.write","document\\.writeln",
-    "javascript:","innerHTML","eval","document\\.cookie"]; 
-/*list for sources which is the input where 
-payload is executed*/
-const sources=["href","src","script ","oneerror=","onmouseover"];
+const websiteLinks=["https://raw.githubusercontent.com/adham-hashem/XSS-payloads/main/Payloads.txt"];
+let retrieved=false;
+
+
+function getDataSet(url){
+    //datasets numbered according to how many links there are
+        let payloadDataSet;
+    //trying to get data from website
+    try{
+        for(let a=0;a<url.length;a++){ //for loop to loop through websites to collect data set of dom xss payloads
+            fetch(url[a])
+            .then(response=>response.text())
+            .then(data=>{
+                
+                    console.log("Succesful getting payload dataset added to list from",url[a])
+                    //divides string into list
+                    //displaying the data as a list due to split function and storing the list
+                    payloadDataSet=data.split("\n");
+                    //console.log(payloadDataSet);
+                   
+
+            })
+
+        }
+        
+    }catch (error){
+        alert("Error fetching dom xss payload dataset")
+    }
+}
 
 //get HTML content of web page
-const HTMLContent=document.documentElement.innerHTML;
+//const HTMLContent=document.documentElement.innerHTML;
 //These make the webpage to display url and cookie infor
 //and the string
 console.log("Testing this function");
 
-function detectSources(HTMLContent,sources){
+function detectSources(){
     //try catch error
    try{
-    //filters sources that are found into a list
-    const foundSources=sources.filter(sources=>HTMLContent.includes(sources));
-    //logs found sources
-    //console.log(foundSources);
-    return foundSources;
+    
+   
    }catch(error){
     console.error(error);
    }
 }
-detectSources(HTMLContent,sources);
-
-function contentInSources(list,HTMLContent){
-    
-}
-let li=detectSources(HTMLContent,sources);
-contentInSources(li);
+detectSources();
+getDataSet(websiteLinks);
