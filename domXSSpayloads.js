af2d3208222438ccfd62d7d3e81f806d230e0a8c
Obodo <aobodo3@gmail.com>
2025-12-14 16:17:39 +0000

    Update domXSSpayloads.js
    This fetches payload from the website link and gives a message if data has been received. There is some code about the indexDB. Just started trying to make it work but have not finished it.

--- domXSSpayloads.js
+++ domXSSpayloads.js
@@ -1,30 +1,25 @@
 /*this file fetches dom xss payloads
-using fetch command*/
-let payloads=[];
-const websiteLinks=["https://github.com/adham-hashem/XSS-payloads/blob/main/Payloads.txt"];
+using fetch command.*/
+
+const websiteLinks=["https://raw.githubusercontent.com/adham-hashem/XSS-payloads/main/Payloads.txt"];
 let retrieved=false;
 
 
 function getDataSet(url){
-  //trying to get data from website
+    //datasets numbered according to how many links there are
+        let payloadDataSet;
+    //trying to get data from website
     try{
-        for(let a=0;a<websiteLinks.length;a++){ //for loop to loop through websites to collect data set of dom xss payloads
+        for(let a=0;a<url.length;a++){ //for loop to loop through websites to collect data set of dom xss payloads
             fetch(url[a])
             .then(response=>response.text())
             .then(data=>{
-                //logs data received
-                //console.log(data);
                 
-                //console.log("Succesful getting payload dataset")
-                /*checking if dataset exists*/
-                if(url[0].includes(data)){
-                    console.log("Data is already included")
-                }else if(!url[a].includes(data)){
-                    //add data to the array
-                    payloads.push(data);
-                    console.log("Succesful getting payload dataset added to list")
-                    //console.log(data);   //just displaying the data
-            }
+                    console.log("Succesful getting payload dataset added to list from",url[a])
+                    //divides string into list
+                    //displaying the data as a list due to split function and storing the list
+                    payloadDataSet=data.split("\n");
+                    console.log(payloadDataSet);
 
             })
 
@@ -35,6 +30,20 @@ function getDataSet(url){
     }
 }
 
-getDataSet(websiteLinks);
+/*will try to store dataset into a large database
+using the indexed DB database
+will request to open the database*/
+/*function openDataSet(){
+    let db;
+    const request=window.indexedDB.open("PayloadsDatabase");
+    request.onerror=(event)=>{
+        console.error("Not allowed to use IndexedDB");
+    };
+    request.onsuccess=(event)=>{
+        db=event.target.result;
+        console.log("success")
+    };
+}*/
 
-//console.log("List of payloads",payloads)
+//openDataSet();
+getDataSet(websiteLinks);
