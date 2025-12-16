e7d8d2e31a32f49dfe8c371715a9fb4f0ac19ad8
Obodo <aobodo3@gmail.com>
2025-12-16 17:58:36 +0000

    Update domDetector.js
    so far i have written code that can check which elements use the sources in the arrays and it shows important data but i need to find a way to read through the data which was stored in a functions return statement.

--- domDetector.js
+++ domDetector.js
@@ -1,31 +1,30 @@
-/*sinks for wehre the attack occurs*/
-const htmlSinks = [
-    "href",            // For <a href=''>
-    "src",             // For <img src=''>, <iframe src=''>
-    "src",             // Could also include <iframe src=''>
-    "innerHTML",      // For setting HTML content
-    "outerHTML",      // For outer HTML manipulation
-    "document.write"   // Directly writing to HTML
+/*sinks for wehre the attack occurs
+
+[""] allows it look for each element that uses the source*/
+
+const htmlSources = [
+    "[href]",            // For <a href=''>
+    "[src]",             // For <img src=''>, <iframe src=''>
+    "[src]",             // Could also include <iframe src=''>
+    "[innerHTML]",      // For setting HTML content
+    "[outerHTML]"      // For outer HTML manipulation
 ];
 
-const jsSinks = [
-    "eval",           // Executes a string as JavaScript
-    "setTimeout",     // Executes code after a delay, can take a string
-    "setInterval",    // Repeats execution of code at intervals, can take a string
-    "innerHTML",      // Can set HTML content from user input
-    "outerHTML",      // Similar to innerHTML but affects the entire element
-    "document.write",  // Writes HTML directly
-    "onclick",        // Inline event handler
-    "onload"         // Inline event handler for window load
+const jsSources = [
+    "[eval]",           // Executes a string as JavaScript
+    "[setTimeout]",     // Executes code after a delay, can take a string
+    "[setInterval]",    // Repeats execution of code at intervals, can take a string
+    "[onclick]",        // Inline event handler
+    "[onload]"         // Inline event handler for window load
 ];
-
-const cssSinks = [
+//css may not work so i will stick with html and javascript for now
+const cssSources = [
     "background-image", // Can take URLs, potentially dangerous
     "expression",       // (Only in older IE versions)
     "style"             // Any inline style attribute that can take user input
 ];
 
-
+const sourceCalls={};
 
 
 function isItSuspicous(input){
@@ -38,13 +37,29 @@ function isItSuspicous(input){
    }
 }
 
-function searchSources(elements){
-    elements.forEach(element => {
-        //display tag name of each source
-        console.log("Found",element.tagName)
-    });
+let foundSources=[];
+let htmlSet={};
+/*this stores the sources as well as the elemnts that use them */
+function searchForSources(sources){
+    /*loop through each item in the source list*/
+    //getting all
+    let allElements;
+    for(x=0;x<sources.length;x++){
+        allElements=document.querySelectorAll("*"+sources[x])
+        foundSources.push(allElements);
+    }
+    return foundSources //stores the values in the array that stores node lists
 }
-searchSources(sources);
+
+
+let htmlHolder=searchForSources(htmlSources); //THIS ALSO stores the values in the array that stores node lists
+//using flat function with INIFINITY combine sub arrays into one array
+console.log("Elements that use the html sources",htmlHolder.flat(Infinity));  //does not flatten the way i wanted it TO
+let javascriptHolder=searchForSources(jsSources);
+//console.log("Elements that use the following javascript source",javascriptHolder);
+
 //getDataSet(websiteLinks);
 const test='alert("XSS")';
+
+
 
