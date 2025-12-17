59a1ca7a5f88520d2f32fddd2fa2d161919a3861
Obodo <aobodo3@gmail.com>
2025-12-17 10:32:55 +0000

    Update domDetector.js
    Made the elemnts of the sources into  a html caetgory and javascript category. Next will need to check the elements and there sources based on the sources list of html or javascript to see if they contain anay dangerous code.

--- domDetector.js
+++ domDetector.js
@@ -1,11 +1,10 @@
 /*sinks for wehre the attack occurs
 
-"[]" allows it look for each element that uses the source*/
+[""] allows it look for each element that uses the source*/
 
 const htmlSources = [
     "[href]",            // For <a href=''>
     "[src]",             // For <img src=''>, <iframe src=''>
-    "[src]",             // Could also include <iframe src=''>
     "[innerHTML]",      // For setting HTML content
     "[outerHTML]"      // For outer HTML manipulation
 ];
@@ -15,7 +14,7 @@ const jsSources = [
     "[setTimeout]",     // Executes code after a delay, can take a string
     "[setInterval]",    // Repeats execution of code at intervals, can take a string
     "[onclick]",        // Inline event handler
-    "[onload]"         // Inline event handler for window load
+    "[onload]"        // Inline event handler for window load
 ];
 //css may not work so i will stick with html and javascript for now
 const cssSources = [
@@ -37,30 +36,58 @@ function isItSuspicous(input){
    }
 }
 
-let foundSources=[];
-let htmlSet={};
-/*this stores the sources as well as the elemnts that use them */
-function searchForSources(sources){
+//these only store one array with node lists i want to make each source have its own seperate array to compare
+const foundHtmlSources=[]; 
+const foundJavascriptSources=[];
+
+/*this stores the sources as well as the elemnts that use them but that contains an array with many arrays inside the one array
+so i try it make easier to  manage*/
+function searchForSources(sources,li){
     /*loop through each item in the source list*/
     //getting all
     let allElements;
     for(x=0;x<sources.length;x++){
         allElements=document.querySelectorAll("*"+sources[x])
-        foundSources.push(allElements);
+        li.push(allElements);
     }
-    return foundSources //stores the values in the array that stores node lists
+    return li //stores the values in the array that stores node lists
 }
 
 
-let htmlHolder=searchForSources(htmlSources); //THIS ALSO stores the values in the array that stores node lists
+let htmlHolder=searchForSources(htmlSources,foundHtmlSources); //THIS ALSO stores the values in the array that stores node lists
 //using flat function with INIFINITY combine sub arrays into one array
-console.log("Elements that use the html sources",htmlHolder.flat(Infinity));  //does not flatten the way i wanted it TO
-let javascriptHolder=searchForSources(jsSources);
+//console.log("Elements that use the html sources",htmlHolder.flat(Infinity));  //does not flatten the way i wanted it TO
+let javascriptHolder=searchForSources(jsSources,foundJavascriptSources);
 //console.log("Elements that use the following javascript source",javascriptHolder);
+//console.log("Elements that use the following html source",htmlHolder);
+
+
+//trying break down list into seperate list
+const seperateHtmlArray=[];
+const seperateJavascriptArray=[];
+
+ function joinNodeLists(sourceItems,sourceHolder,container){
+    for(z=0;z<sourceHolder.length;z++){
+        for(y=0;y<sourceHolder[z].length;y++){
+            //accesses each item in the node list
+            //console.log(sourceHolder[z][y])
+            //storing the node list into a seperate list to make them easy to check
+            //check if a node list is empty
+            
+            container.push(sourceHolder[z][y]);
+            
+
+        }
+    }
+    return container;
+}
+let jsElements=joinNodeLists(jsSources,javascriptHolder,seperateJavascriptArray);
+let htmlElements=joinNodeLists(htmlSources,htmlHolder,seperateHtmlArray);
+console.log("Here is the array combined with the node lists length of javascript elements",jsElements);
+console.log("Here is the array combined with the node lists length of html elements",htmlElements);
 
 //getDataSet(websiteLinks);
 const test='alert("XSS")';
-
 
 
 
