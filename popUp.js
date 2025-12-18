defea2ced3a5c5b65e7742d726a20fbaeb342c55
Obodo <aobodo3@gmail.com>
2025-12-18 15:16:59 +0000

    Create popUp.js
    Added events for the buttons, but the event is not working.

---
+++ popUp.js
@@ -0,0 +1,13 @@
+/*adding event listeners for the buttons
+ */
+//for ON BUTTON
+document.getElementById("b2").addEventListener("DOMContentLoaded",function(){
+    alert("Checking if OFF button works");
+});
+
+//for OFF BUTTON
+document.getElementById("b1").addEventListener("DOMContentLoaded",function(){
+    alert("Checking if OFF button works");
+});
+
+
