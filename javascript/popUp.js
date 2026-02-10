ee8e32c23db0e5f96a7ef3f4024b04952ecf5217
Obodo <aobodo3@gmail.com>
2026-02-10 16:11:56 +0000

    Update popUp.js
    Made a new title that shows the number of times an XSS payload has been found as well as a new button that changes the value when clicked on.

--- javascript/popUp.js
+++ javascript/popUp.js
@@ -1,26 +1,36 @@
- /*This should have worked but because
- the extension is loaded as a temporary extension the maagement API
- does not work*/
+ /*This does not work cause the extension is loaded temporariliy*/
+ 
+ function updateCounter(){
+    let counterElement=document.getElementById("counter");
+            //display current value
+    counterElement.innerHTML=window.counter; //changed the value
+    }
+
+   window.updateCounter=updateCounter;
+ 
  document.addEventListener("DOMContentLoaded",function(){
+    updateCounter();
         const b1 = document.getElementById("b1"); // ON button
         const b2 = document.getElementById("b2"); // OFF button
+        const b3=document.getElementById("b3"); //refresh number button
 
         //set extension id
         const extensionId="sample@example.com";
+    
         // Add event listener for the ON button
     b1.onclick = function() {
         //get extension details
-        chrome.management.get(extensionId,function(extensionInfo){
-        if (chrome.runtime.lastError) {
+        browser.management.get(extensionId,function(extensionInfo){
+        if (browser.runtime.lastError) {
             alert("Error retrieving extension details: " + chrome.runtime.lastError.message);
         }
 
         if(extensionInfo.disabled){//checks if it is disabled
-            chrome.management.setEnabled(extensionId,true,function(){
+            browser.management.setEnabled(extensionId,true,function(){
                  if (chrome.runtime.lastError) {
             alert("Error retrieving extension details: " + chrome.runtime.lastError.message);
         } else{
-            alert("Extension has been enabled");
+            alert("Extension has been enabled"); //enable extension
         }
             });
         }
@@ -31,23 +41,49 @@
     // Add event listener for the OFF button
     b2.onclick = function() {
          //get extension details
-        chrome.management.get(extensionId,function(extensionInfo){
+        browser.management.get(extensionId,function(extensionInfo){
         if (chrome.runtime.lastError) {
             alert("Error retrieving extension details: " + chrome.runtime.lastError.message);
         }
 
         if(extensionInfo.enabled){//checks if it is disabled
-            chrome.management.setEnabled(extensionId,false,function(){
+            browser.management.setEnabled(extensionId,false,function(){
                  if (chrome.runtime.lastError) {
             alert("Error retrieving extension details: " + chrome.runtime.lastError.message);
         } else{
-            alert("Extension has been disabled");
+            alert("Extension has been disabled"); //disable extension
         }
             });
         }
 
-        });
-        
+        }); 
     };
-    
+    function update(num){
+           if(num==null){
+                alert("Num has no value")
+            }else{
+                document.getElementById('counter').innerHTML=num;
+            }
+        }
+    b3.onclick=function (){
+        
+        let previousValue=document.getElementById('counter').innerHTML;
+
+        //get currecnt value from chrome storage insetad of local storage
+        chrome.storage.local.get(['counter'],function(result){
+            let currentValue=result.counter; //default 0 if not set
+            if(currentValue!==previousValue){
+                update(currentValue);//update current value
+
+                //send message to popup if it is open
+                chrome.runtime.sendMessage({type:'UPDATE_COUNTER',counter:currentValue});
+            } else{
+                alert("Values are the same");
+            }
+        });
+    }
+        
     });
+
+    
+    
